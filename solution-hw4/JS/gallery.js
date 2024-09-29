de4457b7efd7f61adef451644c5949f9536c4dbd
TNisha-code <tanishas@andrew.cmu.edu>
2024-09-29 17:11:10 -0400

    HW4 - Create New Detail Page with Arrary

---
+++ solution-hw4/JS/gallery.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.flex-ImageGallery a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Stops default link
+            const rollType = this.getAttribute('data-roll'); // Get roll type 
+            const detailPage = 'ProductDetailsPage.html'; 
+            const newUrl = `${detailPage}?roll=${encodeURIComponent(rollType)}`; // Create new URL/Address 
+            window.location.href = newUrl; // New page 
+        });
+    });
+});
